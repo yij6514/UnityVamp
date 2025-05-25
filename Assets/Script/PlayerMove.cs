04ff91ff2bd3da9a4e91696e65e4a92915db1153
yij6514 <yij6514@gmail.com>
2025-05-26 01:56:51 +0900

    animation

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -4,11 +4,13 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float mSpeed = 5.0f;
     private Camera cam;
+    private Animator anime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = Camera.main;
+        anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,5 +29,22 @@ public class PlayerMove : MonoBehaviour
         mouse.z = d;
         Vector3 worldMousePos = cam.ScreenToWorldPoint(mouse);
         Debug.Log(worldMousePos);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anime.Play("Walking");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            anime.Play("Walking Backwards");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anime.Play("Jumping");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anime.Play("Crouching");
+        }
     }
 }
